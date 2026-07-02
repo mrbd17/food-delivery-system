2b806d6b9baf45ad2f764885f09fabb4685a1f12
Mahmoud <mrbd200719@gmail.com>
2026-07-02 09:18:10 +0300

    building ci.yml file

--- apps/menu/urls.py
+++ apps/menu/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("api/menu/", views.GetMenuView.as_view(), name="menu_api"),
     path("menu/", views.menu, name="menu"),
+    path("api/health/", views.health_check, name="health")
 ]
