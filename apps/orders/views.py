718aaa450dc83dfdf489bd2063df934b19a9eca9
Mahmoud <mrbd200719@gmail.com>
2026-07-03 19:16:21 +0300

    make sure push reach repo

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -8,7 +8,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from .models import Address, Order
 from .order_service import cancel_order, create_order, update_order_status
 from .serializers import (
@@ -57,9 +56,6 @@ class DetectCountryAPIView(APIView):
 
         except:
             return Response({"country_code": "EG"})
-
-
-# Create your views here.
 
 
 def checkoutpage(request):
